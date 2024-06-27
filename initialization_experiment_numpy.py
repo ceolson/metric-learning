33e247769dc7ec1b73045ce259f103508be36f72
ceolson <ceolson00@gmail.com>
2024-06-27 16:34:37 -0400

    rewrite initialization experiment

--- initialization_experiment_numpy.py
+++ initialization_experiment_numpy.py
@@ -2,9 +2,8 @@ import numpy as np
 from folktables import ACSDataSource, ACSMobility
 from matplotlib import pyplot as plt
 import torch
-import cupy as cp
-from cupyx.scipy import stats
-from cupyx.scipy.sparse.linalg import lobpcg
+from scipy import stats
+from scipy.sparse.linalg import lobpcg
 import pandas as pd
 
 def m_dist2(x1, x2, K):
@@ -14,9 +13,9 @@ def generate_synthetic_data(n, r, p, data, S=None):
     X = data.sample(n, axis=0)
     X = X.loc[:, X.var(axis=0) > 0]
     X = X.sample(p, axis=1)
-    X = cp.array(stats.zscore(cp.array(X), axis=0))
+    X = np.array(stats.zscore(np.array(X), axis=0))
    
-    Astar = cp.random.normal(0, 1, size=(p, r)) / cp.sqrt(p)
+    Astar = np.random.normal(0, 1, size=(p, r)) / np.sqrt(p)
     Kstar = Astar @ Astar.T
 
     if not S:
@@ -26,34 +25,34 @@ def generate_synthetic_data(n, r, p, data, S=None):
                 for k in range(n):
                     if i != j and j != k and k != i:
                         Sbar.append((i, j, k))
-        Sbar = cp.array(Sbar)
+        Sbar = np.array(Sbar)
     
-        choices = cp.random.choice([False, True], size=len(Sbar), replace=True, p=[0.7, 0.3])
+        choices = np.random.choice([False, True], size=len(Sbar), replace=True, p=[0.95, 0.05])
         S = Sbar[choices, :]
 
     M = []
     for t in S:
         i, j, k = t
-        Mt = cp.outer(X[i], X[k]) + cp.outer(X[k], X[i]) - cp.outer(X[i], X[j]) - cp.outer(X[j], X[i]) + cp.outer(X[j], X[j]) - cp.outer(X[k], X[k])
+        Mt = np.outer(X[i], X[k]) + np.outer(X[k], X[i]) - np.outer(X[i], X[j]) - np.outer(X[j], X[i]) + np.outer(X[j], X[j]) - np.outer(X[k], X[k])
         M.append(Mt)
-    M = cp.array(M)
+    M = np.array(M)
 
-    probs = 1 / (1 + cp.exp(-cp.einsum('ijj->i', cp.einsum('ijk,kl->ijl', M,  Kstar))))
+    probs = 1 / (1 + np.exp(-np.einsum('ijj->i', np.einsum('ijk,kl->ijl', M,  Kstar))))
 
-    y = 2 * cp.array(probs > cp.random.random(cp.shape(probs)), dtype=int) - 1
+    y = 2 * np.array(probs > np.random.random(np.shape(probs)), dtype=int) - 1
     
     # y = []
     # for t in S:
     #     i, j, k = t
-    #     prob = 1 / (1 + cp.exp(m_dist2(X[i], X[k], Kstar) - m_dist2(X[i], X[j], Kstar)))
-    #     yt = cp.random.choice([1, -1], size=1, p=[prob, 1 - prob])
+    #     prob = 1 / (1 + np.exp(m_dist2(X[i], X[k], Kstar) - m_dist2(X[i], X[j], Kstar)))
+    #     yt = np.random.choice([1, -1], size=1, p=[prob, 1 - prob])
     #     y.append(yt)
-    # y = cp.array(y)
+    # y = np.array(y)
     
     return X, S, y, Astar, Kstar
 
 def initialization(n, p, S, X, y):
-    transition_matrices = [cp.zeros((n - 1, n - 1)) for _ in range(n)]
+    transition_matrices = [np.zeros((n - 1, n - 1)) for _ in range(n)]
     for t in range(len(S)):
         def gap(l, i):
             if l < i:
@@ -61,76 +60,66 @@ def initialization(n, p, S, X, y):
             else:
                 return l - 1
         i, j, k = S[t]
-        i = i.get()
-        j = j.get()
-        k = k.get()
         transition_matrices[i][gap(j, i), gap(k, i)] = 0.99 if y[t] == 1 else 0.01
         transition_matrices[i][gap(k, i), gap(j, i)] = 0.99 if y[t] == -1 else 0.01
 
     for i in range(n):
-        d = cp.max(cp.sum(transition_matrices[i], axis=1))
+        d = np.max(np.sum(transition_matrices[i], axis=1))
         transition_matrices[i] = transition_matrices[i] / d
 
-        self_loops = cp.diag(1 - cp.sum(transition_matrices[i], axis=1))
+        self_loops = np.diag(1 - np.sum(transition_matrices[i], axis=1))
 
         transition_matrices[i] += self_loops
 
     dists_from_i = []
     for i in range(n):
-        eigenvalues, eigenvectors = cp.linalg.eigh(transition_matrices[i])
-        leading_index = cp.where(cp.isclose(eigenvalues, 1))[0]
-        leading_eigenvector = cp.abs(eigenvectors[:, leading_index].real)
-        dists_wo_i = cp.log(leading_eigenvector)
-        if not cp.all(cp.isfinite(dists_wo_i)):
+        eigenvalues, eigenvectors = np.linalg.eigh(transition_matrices[i])
+        leading_index = np.where(np.isclose(eigenvalues, 1))[0]
+        leading_eigenvector = np.abs(eigenvectors[:, leading_index].real)
+        dists_wo_i = np.log(leading_eigenvector)
+        if not np.all(np.isfinite(dists_wo_i)):
             print(i)
-        dists = cp.zeros(n)
-        cp.put(dists, list(range(i)) + list(range(i+1, n)), dists_wo_i)
+        dists = np.zeros(n)
+        np.put(dists, list(range(i)) + list(range(i+1, n)), dists_wo_i)
         dists_from_i.append(dists)
-    D = cp.stack(dists_from_i)
+    D = np.stack(dists_from_i)
 
-    J = cp.identity(n) - (cp.ones((n, 1)) @ cp.ones((1, n))) / n
+    J = np.identity(n) - (np.ones((n, 1)) @ np.ones((1, n))) / n
     H = - J @ D @ J / 2
     Xprime = J @ X
     
     XtHX = Xprime.T @ H @ Xprime / (n**2)
     Sigma = Xprime.T @ Xprime / n
-    eigenvalues, eigenvectors = lobpcg(A=XtHX, B=Sigma, X=cp.random.normal(size=(p,r)))
-    U = eigenvectors[:, cp.argsort(eigenvalues)[-r:]]
-    Lambda = cp.sort(eigenvalues)[-r:]
-    AAt = U @ cp.diag(Lambda) @ U.T
+    eigenvalues, eigenvectors = lobpcg(A=XtHX, B=Sigma, X=np.random.normal(size=(p,r)))
+    U = eigenvectors[:, np.argsort(eigenvalues)[-r:]]
+    Lambda = np.sort(eigenvalues)[-r:]
+    AAt = U @ np.diag(Lambda) @ U.T
     
-    V, s, W = cp.linalg.svd(AAt)
+    V, s, W = np.linalg.svd(AAt)
     
-    top_r_indicies = cp.argsort(s)[:r]
+    top_r_indicies = np.argsort(s)[:r]
     V_r = V[:, top_r_indicies].reshape((p, r))
     s_r = s[top_r_indicies]
-    Ahat = V_r @ cp.sqrt(cp.diag(s_r))
+    Ahat = V_r @ np.sqrt(np.diag(s_r))
 
     return Ahat
 
-def L(A, triplets): 
-    yMtAs = torch.einsum('bij,jk->bik', triplets, A)
-    yMtAATs = torch.einsum('bij,jk->bik', yMtAs, torch.transpose(A, 0, 1))
-    TryMtAATs = torch.einsum('bii->b', yMtAATs)
-    losses = torch.log(1 + torch.exp(-TryMtAATs))
-    
-    return torch.mean(losses)
 
-synthetic_data = pd.DataFrame(cp.random.normal(size=(1000, 50)).get())
+synthetic_data = pd.DataFrame(np.random.normal(size=(1000, 50)))
 
 r = 3
-p = 20
+p = 30
 
-for n in range(100, 400, 20):
+for n in [250, 500, 750, 1000, 1250]:
     
     print("Generating synthetic data...")
     X, S, y, Astar, Kstar = generate_synthetic_data(n, r, p, synthetic_data)
-    print(cp.shape(X))
+    print(np.shape(X))
     
-    cp.save("Astar.npy", Astar.get())
+    np.save("Astar.npy", Astar)
     
     print("Initializing...")
     A0 = initialization(n, p, S, X, y)
     
     print(n)
-    print(cp.linalg.norm(Astar - A0))
+    print(np.linalg.norm(Astar - A0))
--- initialization_experiment_script.sh
+++ initialization_experiment_script.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 #SBATCH --account=sur_lab
-#SBATCH --job-name=jupyter
-#SBATCH --partition=gpu
+#SBATCH --job-name=initialization_experiment
+#SBATCH --partition=bigmem
+#SBATCH --time=3-00:00
+#SBATCH --mem=2000G
+#SBATCH -c 64
+#SBATCH --output=/n/home10/colson/metric-learning/out_files/%j.out
+#SBATCH --error=/n/home10/colson/metric-learning/out_files/%j.err
+#SBATCH --open-mode=truncate
+#SBATCH --mail-type=BEGIN
+
+##SBATCH --cpus-per-task=64
 ##SBATCH --gres=gpu:nvidia_a40:2
 ##SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1
 ##SBATCH --gres=gpu:tesla_v100-pcie-32gb:1
-#SBATCH --gres=gpu:4
-#SBATCH --time=1-00:00
-#SBATCH --mem=1000G
-#SBATCH --cpus-per-task=64
-#SBATCH --output=/n/home10/colson/metric-learning/out_files/%x.out
-#SBATCH --error=/n/home10/colson/metric-learning/out_files/%x.err
-#SBATCH --open-mode=truncate
-#SBATCH --mail-type=BEGIN
+##SBATCH --gres=gpu:4
 
 # Load modules
 module load python/3.10.9-fasrc01
@@ -20,4 +22,4 @@ module load cuda/11.8.0-fasrc01
 module load cudnn/8.9.2.26_cuda11-fasrc01
 mamba activate metriclearning2
 
-python3 initialization_experiment.py
+python3 initialization_experiment_numpy.py
